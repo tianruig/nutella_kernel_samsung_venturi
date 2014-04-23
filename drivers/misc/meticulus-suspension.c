56a4e7921c92c8f189d8cde5997bdd9867be1f2e
Meticulus <theonejohnnyd@gmail.com>
2014-04-23 12:47:19 -0500

    meticulus-suspension: Update 1.1
    -Reworked the entire process.
    
    -Process is now like this:
    1. On screen off store cpu freq min and max.
    2. On suspend set cpu freqs min 400Mhz, max 1000Mhz.
    3. Init wakelock to cancel suspend and let new freqs settle.
    4. Suspend retries in about a minute and should go through.
    5. On Screen on restore saved cpu freq min and max.

--- drivers/misc/meticulus-suspension.c
+++ drivers/misc/meticulus-suspension.c
@@ -16,71 +16,51 @@
 #include <linux/sched.h>
 #include <linux/earlysuspend.h>
 #include <linux/module.h>
+#include <linux/wakelock.h>
 
-#define SLEEP_FREQ	(800 * 1000) /* Use 800MHz when entering sleep */
-
+/* print debug messages */
 static bool debug = true;
 
-static bool enable_screenoff_freqs = false;
-
+/* flag to insure that somethings only happen */
+/* once in the suspend resume process */
 static bool sleep_freq_is_set = false;
 
-/* Placeholder for policy values during suspend */
+/* storage for policy values during suspend */
 static int max,min,usermax,usermin = 0;
 
-static int screenoff_freq_min, screenoff_freq_max = SLEEP_FREQ;
+/* frequencies to use for suspend */
+static int sleep_freq_min = (400 * 1000);
+static int sleep_freq_max = (1000 * 1000);
 
-enum cpufreq_access {
-	DISABLE_FURTHER_CPUFREQ = 0x10,
-	ENABLE_FURTHER_CPUFREQ = 0x20,
-};
+/* time to let new sleep freqs to settle */
+static long defer_sleep_timeout = 20000;
 
-static void meticulus_wait_for_target_freq(void)
-{
-	while(cpufreq_quick_get(0) < SLEEP_FREQ) {
+static struct wake_lock defer_suspend;
 
-		if(debug)
-			printk("[MET_SUS]: CPU FREQ = %d\n",cpufreq_quick_get(0));
-		
-		mdelay(10);
-	}
-	if(debug)
-		printk("[MET_SUS]: CPU FREQ = %d\n",cpufreq_quick_get(0));
+
+static void meticulus_init_wakelock(void)
+{	
+	wake_lock_init(&defer_suspend,WAKE_LOCK_SUSPEND,"defer_suspend");
 }
 
-static void meticulus_set_screenoff_freq(void)
+static void meticulus_lock_wakelock(void)
 {
-	struct cpufreq_policy *policy = cpufreq_cpu_get(0);
-	policy->max = screenoff_freq_max;
-	policy->min = screenoff_freq_min;
-	policy->user_policy.min = policy->min;
-	policy->user_policy.max = policy->max;
-	cpufreq_update_policy(0);
+	wake_lock_timeout(&defer_suspend, defer_sleep_timeout);
+}
+static void meticulus_release_wakelock(void)
+{
+	wake_unlock(&defer_suspend);
+	wake_lock_destroy(&defer_suspend);
 }
 
 static void meticulus_set_sleep_freq(void)
 {
 	struct cpufreq_policy *policy = cpufreq_cpu_get(0);
-	int ret = -1;
-	int retries = 0;
-
-	policy->max = SLEEP_FREQ;
-	policy->min = SLEEP_FREQ;
+	policy->max = sleep_freq_max;
+	policy->min = sleep_freq_min;
 	policy->user_policy.min = policy->min;
 	policy->user_policy.max = policy->max;
-
-	while(retries < 10) {
-		ret = cpufreq_driver_target(policy, SLEEP_FREQ,
-				DISABLE_FURTHER_CPUFREQ);
-		if(debug)		
-			printk("[MET_SUS]: Trying to set policy - tries=%d\n", retries +1);
-
-		if (ret >= 0) 
-			break;
-
-		mdelay(10);
-		retries += 1;
-	}
+	cpufreq_update_policy(0);
 	sleep_freq_is_set = true;
 }
 
@@ -101,12 +81,11 @@ static void meticulus_restore_freqs(void)
 {
 	struct cpufreq_policy *policy = cpufreq_cpu_get(0);
 
-	cpufreq_driver_target(policy, SLEEP_FREQ,
-			ENABLE_FURTHER_CPUFREQ);
 	policy->max = max;
 	policy->user_policy.max = usermax;
 	policy->min = min;
 	policy->user_policy.min = usermin;
+	cpufreq_update_policy(0);
 	if(debug)
 		printk("[MET_SUS]: Restoring policy values min=%d max=%d usermin=%d usermax=%d\n",
 			min,max,usermin,usermax);
@@ -116,15 +95,22 @@ static void meticulus_restore_freqs(void)
 static void meticulus_early_suspend_enter(struct early_suspend *h)
 {
 	meticulus_store_freqs();
-	if(enable_screenoff_freqs)
-		meticulus_set_screenoff_freq();
+	meticulus_init_wakelock();
 }
 static void meticulus_suspend_enter(void)
 {
 	/* This function can get fired several times 
 	before suspend completes. */
-	if(!sleep_freq_is_set)
+	if(!sleep_freq_is_set || cpufreq_quick_get(0) < sleep_freq_min)
+	{
 		meticulus_set_sleep_freq();
+		/* Activating a wake lock at this point 
+		causes suspend to abort. We are doing 
+		this to give some time for sleep freqs
+		to settle. Suspend will try again when
+		wakelock expires */
+		meticulus_lock_wakelock();
+	}
 }
 static void meticulus_resume_complete(void)
 {
@@ -134,6 +120,7 @@ static void meticulus_resume_complete(void)
 static void meticulus_late_resume_complete(struct early_suspend *h)
 {
 	meticulus_restore_freqs();
+	meticulus_release_wakelock();
 }
 
 static int meticulus_suspend_notifier_event(struct notifier_block *this,
@@ -175,9 +162,6 @@ int __init meticulus_suspend_init(void)
 }
 
 module_param_named(debug, debug, bool, S_IRUGO | S_IWUSR);
-module_param_named(enable_screenoff_freqs, enable_screenoff_freqs, bool, S_IRUGO | S_IWUSR);
-module_param_named(screenoff_freq_min, screenoff_freq_min, int, S_IRUGO | S_IWUSR);
-module_param_named(screenoff_freq_max, screenoff_freq_max, int, S_IRUGO | S_IWUSR);
 
 module_init(meticulus_suspend_init);
 
